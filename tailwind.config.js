53444f23fa2dac522400b0a5b98a7e397d8e7d53
CPuPush <fpakpahan44@gmail.com>
2023-05-01 00:18:39 +0700

    porto

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+        secondary: "#64748b",
+      }
+    },
+  },
+  plugins: [],
+}
+
